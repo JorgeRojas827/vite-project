627d07aa2c6796caad7c40e5839dd2a33c1c0963
eviediaz <evieediaz@icloud.com>
2023-06-25 07:11:18 -0500

    xd
    xd

--- src/MyEditor.tsx
+++ src/MyEditor.tsx
@@ -1,19 +1,33 @@
-import React from 'react';
+import React, {useRef, useLayoutEffect} from 'react';
 import 'ace-builds/src-noconflict/ace';
 import 'ace-builds/esm-resolver';
 import AceEditor from 'react-ace';
 import 'ace-builds/src-noconflict/mode-c_cpp';
 import 'ace-builds/src-noconflict/theme-github.js';
+import { useAceEditor } from './hooks/useAceEditor';
 
 
 function MyEditor() {
     const onChange = (newValue: any) => {
       console.log('Editor content:', newValue);
     };
+    const editor = useRef<any>(null)
+    const { setline } = useAceEditor({ref: editor});
+
+    useLayoutEffect(() => {
+      const interval = setInterval(() => {
+        setline(3)
+      }, 2000)
+      
+      return () => {
+        clearInterval(interval)
+      }
+    }, [])
 
     return (
         <AceEditor
           mode="c_cpp"
+          ref={editor}
           theme="github"
           value={`${`
           console.log('Hello, World!');
---
+++ src/hooks/useAceEditor.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { useStateCallback } from './useStateCallback'
+import ace from 'ace-builds/src-noconflict/ace';
+
+interface iprops {
+    ref: any
+}
+
+export const useAceEditor = ({ref}: iprops) => {
+    const Range = ace.require('ace/range').Range;
+
+    const setline  = (selected: number) => {
+        
+    const prevMarkers = ref.current.editor.session.getMarkers();
+    if (prevMarkers) {
+        const prevMarkersArr = Object.keys(prevMarkers);
+        for (let item of prevMarkersArr) {
+            ref.current.editor.session.removeMarker(prevMarkers[item].id);
+        }
+    }
+    ref.current.editor.session.addMarker(new Range(selected, selected, selected, 0), "foo", "fullLine");
+   }
+
+  return {
+    setline
+  }
+}
