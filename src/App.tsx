b0d50f956d2a4c5bb3638ee3e8b03ed62c448e1c
jorger-delfosti <jorge.rojas@delfosti.com>
2023-06-25 10:51:11 -0500

    added button & binding

--- src/App.tsx
+++ src/App.tsx
@@ -11,57 +11,45 @@ import {
   Cell,
 } from 'recharts';
 import { useEffect, useRef, useState } from 'react';
+import { IoMdPlay } from 'react-icons/io';
 
 const data = [
   {
     name: 'Page A',
     uv: 4000,
-    amt: 2400,
   },
   {
     name: 'Page B',
     uv: 3000,
-    amt: 2210,
   },
   {
     name: 'Page C',
     uv: 2000,
-    amt: 2290,
   },
   {
     name: 'Page D',
     uv: 2780,
-    amt: 2000,
   },
   {
     name: 'Page E',
     uv: 1890,
-    amt: 2181,
   },
   {
     name: 'Page F',
     uv: 2390,
-    amt: 2500,
   },
   {
     name: 'Page G',
     uv: 3490,
-    amt: 2100,
   },
 ];
 
 function App() {
   const [selectedGraph, setSelectedGraph] = useState('0');
-  const counter = useRef(0);
-  const demoUrl = 'https://codesandbox.io/s/simple-bar-chart-tpz8r';
-  const onChange = (newValue: any) => {
-    console.log('change', newValue);
-  };
 
   useEffect(() => {
     setSelectedGraph((prevState) => (Number(prevState) + 1).toString());
   }, []);
-
 
   return (
     <main className="bg-gray-800 flex p-20 space-x-5 flex-col md:flex-row justify-between w-screen h-screen text-white">
@@ -96,28 +84,33 @@ function App() {
           </BarChart>
         </ResponsiveContainer>
       </div>
-      <div className="md:w-1/2 p-5 rounded-lg h-full"> 
-        <button
-          onClick={() => {
-            if (selectedGraph !== '0') {
-              setSelectedGraph((Number(selectedGraph) - 1).toString());
-            }
-          }}
-          className="bg-indigo-700 mb-10 mr-5 text-white p-3 rounded-md"
-        >
-          Anterior
-        </button>
-        <button
-          onClick={() => {
-            if (selectedGraph !== (data.length - 1).toString()) {
-              setSelectedGraph((Number(selectedGraph) + 1).toString());
-            }
-          }}
-          className="bg-indigo-700 mb-10 text-white p-3 rounded-md"
-        >
-          Siguiente
-        </button>
-      <MyEditor/>          
+      <div className="md:w-1/2 h-full">
+        <div className="p-5 flex space-x-3 items-center rounded-lg ">
+          <button
+            onClick={() => {
+              if (selectedGraph !== '0') {
+                setSelectedGraph((Number(selectedGraph) - 1).toString());
+              }
+            }}
+            className="bg-indigo-700 mb-10 text-white p-3 rounded-md"
+          >
+            Anterior
+          </button>
+          <button
+            onClick={() => {
+              if (selectedGraph !== (data.length - 1).toString()) {
+                setSelectedGraph((Number(selectedGraph) + 1).toString());
+              }
+            }}
+            className="bg-indigo-700 mb-10 text-white p-3 rounded-md"
+          >
+            Siguiente
+          </button>
+          <button className="bg-indigo-700 mb-10 text-white p-3 rounded-md">
+            <IoMdPlay />
+          </button>
+        </div>
+        <MyEditor selectedGraph={selectedGraph} />
       </div>
     </main>
   );
--- src/MyEditor.tsx
+++ src/MyEditor.tsx
@@ -1,4 +1,5 @@
-import React, {useRef, useLayoutEffect} from 'react';
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import React, { useRef, useEffect, FC } from 'react';
 import 'ace-builds/src-noconflict/ace';
 import 'ace-builds/esm-resolver';
 import AceEditor from 'react-ace';
@@ -6,30 +7,27 @@ import 'ace-builds/src-noconflict/mode-c_cpp';
 import 'ace-builds/src-noconflict/theme-github.js';
 import { useAceEditor } from './hooks/useAceEditor';
 
+interface IProps {
+  selectedGraph: any;
+}
 
-function MyEditor() {
-    const onChange = (newValue: any) => {
-      console.log('Editor content:', newValue);
-    };
-    const editor = useRef<any>(null)
-    const { setline } = useAceEditor({ref: editor});
+const MyEditor: FC<IProps> = ({ selectedGraph }) => {
+  console.log(selectedGraph);
 
-    useLayoutEffect(() => {
-      const interval = setInterval(() => {
-        setline(3)
-      }, 2000)
-      
-      return () => {
-        clearInterval(interval)
-      }
-    }, [])
+  const editor = useRef<any>(null);
+  const { setline } = useAceEditor({ ref: editor });
 
-    return (
-        <AceEditor
-          mode="c_cpp"
-          ref={editor}
-          theme="github"
-          value={`${`
+  useEffect(() => {
+    setline(Number(selectedGraph));
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [selectedGraph]);
+
+  return (
+    <AceEditor
+      mode="c_cpp"
+      ref={editor}
+      theme="github"
+      value={`${`
           console.log('Hello, World!');
           using namespace std;
           int main(){
@@ -37,17 +35,18 @@ function MyEditor() {
               aasadasdasdasd
           }
           `}`}
-          onChange={onChange}
-          markers = {[{
-              startRow: 3,
-              startCol: 3,
-              endRow: 3,
-              endCol: 0,
-              className: "test-marker",
-              type: "text"
-            }]}
-        />
-    );
-  }
+      markers={[
+        {
+          startRow: 3,
+          startCol: 3,
+          endRow: 3,
+          endCol: 0,
+          className: 'test-marker',
+          type: 'text',
+        },
+      ]}
+    />
+  );
+};
 
-  export default MyEditor;
+export default MyEditor;
